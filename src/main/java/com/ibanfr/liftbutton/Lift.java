24e8e7c23a658941de3d05c64d04ea489564b054
ibanFR <ibanFR@users.noreply.github.com>
2025-04-08 21:04:19 +0100

    refactor: initialize doorStatus on class constructor

--- src/main/java/com/ibanfr/liftbutton/Lift.java
+++ src/main/java/com/ibanfr/liftbutton/Lift.java
@@ -1,7 +1,14 @@
 package com.ibanfr.liftbutton;
 
 public class Lift {
+
+    private DoorStatus doorStatus;
+
+    public Lift() {
+        doorStatus = DoorStatus.CLOSED;
+    }
+
     public DoorStatus doors() {
-        return DoorStatus.CLOSED;
+        return doorStatus;
     }
 }
