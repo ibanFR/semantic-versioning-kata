7b78f77b3d308e7461589022e6aaeb9ce9b73e82
ibanFR <ibanFR@users.noreply.github.com>
2025-04-19 16:35:03 +0100

    feat: add lights() method to Lift class to query the light status

--- src/main/java/com/ibanfr/liftbutton/Lift.java
+++ src/main/java/com/ibanfr/liftbutton/Lift.java
@@ -3,12 +3,22 @@ package com.ibanfr.liftbutton;
 public class Lift {
 
     private DoorStatus doorStatus;
+    private LightStatus lightStatus;
 
     public Lift() {
         doorStatus = DoorStatus.CLOSED;
+        lightStatus = LightStatus.OFF;
     }
 
     public DoorStatus doors() {
         return doorStatus;
+    }
+
+    public void pressButton() {
+
+    }
+
+    public LightStatus lights() {
+        return lightStatus;
     }
 }
--- src/test/java/com/ibanfr/liftbutton/LiftTest.java
+++ src/test/java/com/ibanfr/liftbutton/LiftTest.java
@@ -1,5 +1,6 @@
 package com.ibanfr.liftbutton;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,6 +9,7 @@ class LiftTest {
 
     //TEST LIST
     //[X] - doors should be CLOSED when Lift is created
+    //[X] - lights should be OFF when Lift is created
     //[] - should switch lights ON when button is pressed
     //[] - should OPEN the lift doors when lift arrives
     //[] - should switch OFF the lights when lift arrives
@@ -28,4 +30,40 @@ class LiftTest {
                 .isEqualTo(DoorStatus.CLOSED);
 
     }
+
+    /**
+     * lights should be OFF when Lift is created
+     */
+    @Test
+    void lights_should_be_OFF_when_Lift_is_created() {
+
+        //when
+        Lift lift = new Lift();
+
+        //then
+        assertThat(lift.lights())
+                .as("lights should be OFF")
+                .isEqualTo(LightStatus.OFF);
+
+    }
+
+    /**
+     * should switch lights ON when button is pressed
+     */
+    @Test
+    @Disabled("Not implemented yet")
+    void should_switch_lights_ON_when_button_is_pressed() {
+        //given
+        Lift lift = new Lift();
+
+        //when
+        lift.pressButton();
+
+        //then
+        assertThat(lift.lights())
+                .as("lights should be ON")
+                .isEqualTo(LightStatus.ON);
+
+    }
+
 }
