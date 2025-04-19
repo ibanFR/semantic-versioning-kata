e605214c9066f2f5ae727fc6469359b1cc845dfc
ibanFR <ibanFR@users.noreply.github.com>
2025-04-19 16:47:54 +0100

    test: mark test as complete

--- src/test/java/com/ibanfr/liftbutton/LiftTest.java
+++ src/test/java/com/ibanfr/liftbutton/LiftTest.java
@@ -10,7 +10,7 @@ class LiftTest {
     //TEST LIST
     //[X] - doors should be CLOSED when Lift is created
     //[X] - lights should be OFF when Lift is created
-    //[] - should switch lights ON when button is pressed
+    //[X] - should switch lights ON when button is pressed
     //[] - should OPEN the lift doors when lift arrives
     //[] - should switch OFF the lights when lift arrives
     //[] - lights should be OFF when button is pressed AND doors are OPEN
