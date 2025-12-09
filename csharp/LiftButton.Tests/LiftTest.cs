c01dfad88f096e14bde3b867cc9a4208965df9ae
ibanFR <ibanFR@users.noreply.github.com>
2025-12-09 23:14:30 +0100

    test: added test list to LiftTest.cs

--- csharp/LiftButton.Tests/LiftTest.cs
+++ csharp/LiftButton.Tests/LiftTest.cs
@@ -2,7 +2,13 @@
 
 public class LiftTests
 {
-    
+    //TEST LIST
+    //[] - doors should be CLOSED when Lift is created
+    //[] - should switch lights ON when button is pressed and doors are CLOSED
+    //[] - should OPEN the lift doors when lift arrives
+    //[] - should switch OFF the lights when lift arrives
+    //[] - lights should be OFF when button is pressed and doors are OPEN
+
     [Test]
     public void Test1()
     {
